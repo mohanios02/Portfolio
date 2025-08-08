600fc56fc1f459c5627dde97321598052d8c0fe1
mohan123 <mohan.k@sunplussoftware.com>
2025-08-08 14:57:23 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        secondary: "#1e293b",
+        accent: "#f43f5e",
+      },
+    },
+  },
+  plugins: [],
+}
